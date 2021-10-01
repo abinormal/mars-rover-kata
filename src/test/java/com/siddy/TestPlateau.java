18129b934444ce49866f8ea37630e6810d18d7bc
Abi Sidford <asidford@gmail.com>
2021-10-01 11:10:21 +0100

    Changed tests to reflect change in the constructor.

--- src/test/java/com/siddy/TestPlateau.java
+++ src/test/java/com/siddy/TestPlateau.java
@@ -19,102 +19,106 @@ public class TestPlateau {
     }
 
     @Test
-    public void assertMinusPlateauValueException() {
+    public void assertMinusPlateauValueExceptionX() {
         String exceptionMessage = "";
         String message = "Plateau size must be greater than zero";
         try {
-            Plateau plateau = new Plateau(-5,12);
-        } catch (Exception exception){
+            Plateau plateau = new Plateau(-5, 12);
+        } catch (Exception exception) {
             exceptionMessage = exception.getMessage();
         }
         assertEquals(message, exceptionMessage);
     }
 
-//
-//    @Test
-//    public void checkYMinus() {
-//        // Return Error
-//        assertEquals(Error.ERROR_INVALID_PLATEAU_SIZE, p.Plateau(15, -27));
-//    }
-//
-//    @Test
-//    public void checkCantMoveNorth() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(4, 6, Direction.NORTH);
-//        }
-//        assertEquals(Error.ERROR_OVER_EDGE, result);
-//    }
-//
-//    @Test
-//    public void checkCantMoveEast() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(5, 3, Direction.EAST);
-//        }
-//        assertEquals(Error.ERROR_OVER_EDGE, result);
-//    }
-//
-//    @Test
-//    public void checkCantMoveSouth() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(5, 0, Direction.SOUTH);
-//        }
-//        assertEquals(Error.ERROR_OVER_EDGE, result);
-//    }
-//
-//    @Test
-//    public void checkCantMoveWest() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(0, 3, Direction.WEST);
-//        }
-//        assertEquals(Error.ERROR_OVER_EDGE, result);
-//    }
-//
-//    @Test
-//    public void checkCanMoveNorth() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(1, 5, Direction.NORTH);
-//        }
-//        assertEquals(Error.NO_ERROR, result);
-//    }
-//
-//    @Test
-//    public void checkCanMoveEast() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(4, 3, Direction.EAST);
-//        }
-//        assertEquals(Error.NO_ERROR, result);
-//    }
-//
-//    @Test
-//    public void checkCanMoveSouth() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(1, 1, Direction.SOUTH);
-//        }
-//        assertEquals(Error.NO_ERROR, result);
-//    }
-//
-//    @Test
-//    public void checkCanMoveWest() {
-//        // Given a value can the vehicle move 1 space in the given direction?
-//        Error result = Error.NO_ERROR;
-//        if (p.Plateau(5, 6) == Error.NO_ERROR) {
-//            result = p.move(1, 3, Direction.WEST);
-//        }
-//        assertEquals(Error.NO_ERROR, result);
-//    }
+    @Test
+    public void assertMinusPlateauValueExceptionY() {
+        String exceptionMessage = "";
+        String message = "Plateau size must be greater than zero";
+        try {
+            Plateau plateau = new Plateau(15, -12);
+        } catch (Exception exception) {
+            exceptionMessage = exception.getMessage();
+        }
+        assertEquals(message, exceptionMessage);
+    }
 
+    @Test
+    public void assertZeroPlateauValueExceptionX() {
+        String exceptionMessage = "";
+        String message = "Plateau size must be greater than zero";
+        try {
+            Plateau plateau = new Plateau(0, 12);
+        } catch (Exception exception) {
+            exceptionMessage = exception.getMessage();
+        }
+        assertEquals(message, exceptionMessage);
+    }
+
+    @Test
+    public void assertZeroPlateauValueExceptionY() {
+        String exceptionMessage = "";
+        String message = "Plateau size must be greater than zero";
+        try {
+            Plateau plateau = new Plateau(15, 0);
+        } catch (Exception exception) {
+            exceptionMessage = exception.getMessage();
+        }
+        assertEquals(message, exceptionMessage);
+    }
+
+    @Test
+    public void checkCantMoveNorth() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.ERROR_OVER_EDGE, p.move(4, 6, Direction.NORTH));
+    }
+
+    @Test
+    public void checkCantMoveEast() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.ERROR_OVER_EDGE, p.move(5, 3, Direction.EAST));
+    }
+
+    @Test
+    public void checkCantMoveSouth() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.ERROR_OVER_EDGE, p.move(5, 0, Direction.SOUTH));
+    }
+
+    @Test
+    public void checkCantMoveWest() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.ERROR_OVER_EDGE, p.move(0, 3, Direction.WEST));
+    }
+
+    @Test
+    public void checkCanMoveNorth() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.NO_ERROR, p.move(1, 5, Direction.NORTH));
+    }
+
+    @Test
+    public void checkCanMoveEast() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.NO_ERROR, p.move(4, 3, Direction.EAST));
+    }
+
+    @Test
+    public void checkCanMoveSouth() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.NO_ERROR, p.move(1, 1, Direction.SOUTH));
+    }
+
+    @Test
+    public void checkCanMoveWest() {
+        // Given a value can the vehicle move 1 space in the given direction?
+        Plateau p = new Plateau(5, 6);
+        assertEquals(Error.NO_ERROR, p.move(1, 3, Direction.WEST));
+    }
 }
