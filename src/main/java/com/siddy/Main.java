0134c0afe268febd3549e649f0c2cd8f19ffb639
Abi Sidford <asidford@gmail.com>
2021-10-11 22:49:30 +0100

    Added get numbers from command line.

--- src/main/java/com/siddy/Main.java
+++ src/main/java/com/siddy/Main.java
@@ -9,12 +9,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to Mars!");
-
-        int[] pSize = getPlateauSize();
+        System.out.println("How large is the plateau? Enter two numbers separated by a space. ");
+        int[] pSize = getCMDNumbers();
         Plateau plateau = new Plateau(pSize[0], pSize[1]);
 
         do {
-            Vehicle rover = new Rover(1, 1, Direction.SOUTH, plateau);
+            System.out.println("Enter the vehicle coordinates (x y) separated by a space");
+            int[] coords = getCMDNumbers();
+            Direction d = Direction.NORTH; //getDirection();
+            Vehicle rover = new Rover(coords[0], coords[1], d, plateau);
 
             System.out.println("Position of the rover: x" + rover.getPosX() + " y" + rover.getPosY() + " " + rover.getDirection());
             Error error = rover.processMovement("MMMLM");
@@ -22,40 +25,45 @@ public class Main {
             if (error == Error.ERROR_BAD_MOVEMENT_STRING) {
                 System.out.println("Movement string can only contain the letters LRM.");
             }
-            System.out.println("Would you like to add another vehicle? y/n");
 
-        } while (addAnotherVehicle()); // todo - fix this to exit on command
+        } while (addAnotherVehicle());
 
     }
-    private static int[] getPlateauSize(){
-        String size = "7 7";
-        // Enter data using BufferReader
 
-        System.out.println("How large is the plateau? Enter two numbers separated by a space. ");
+    private static int[] getCMDNumbers() {
+        String size = "";
+
         try {
-            // Reading data using readLine
             size = reader.readLine();
-            if (!size.matches("^[0-9 ]+$")){
+            if (!size.matches("^[0-9 ]+$")) {
                 System.out.println("Must be a number.");
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         String[] pSize = size.split(" ");
 
         return new int[]{Integer.parseInt(pSize[0]), Integer.parseInt(pSize[1])};
     }
-    private static boolean addAnotherVehicle(){
+
+
+    private static boolean addAnotherVehicle() {
         String response = "n";
-        try {
-            // Reading data using readLine
-            response = reader.readLine();
-            if (!response.matches("^[yn]")){
-                System.out.println("Must be either y or n");
+        boolean haveResponse = false;
+        do {
+            System.out.println("Would you like to add another vehicle? y/n");
+            try {
+                // Reading data using readLine
+                response = reader.readLine();
+                if (!response.matches("^[yn]")) {
+                    System.out.println("Must be either y or n");
+                } else {
+                    haveResponse = true;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-        }
+        } while (!haveResponse);
         return response.equals("y");
     }
 }
